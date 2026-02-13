6b465d3250e87eabe857ee2631ab1154ccaad42a
BrunoShima <bshimabukuro@my.bcit.ca>
2026-02-13 01:11:48 -0800

    Backend wiring 1
    ran into some issues, but currently stable
    
    frontend and backend successfully communicating

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,62 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation, Navigate } from 'react-router-dom'
 import './App.css'
+
 import Navbar from './components/Navbar.jsx'
 import HomePage from './pages/HomePage.jsx'
 import DetailPage from './pages/DetailPage.jsx'
 import SavedMoviesPage from './pages/SavedMoviesPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import RegisterPage from './pages/RegisterPage.jsx'
 
-function App() {
+console.log("testing app")
+
+// Stores current location
+function AppLayout() {
+  const location = useLocation();
+
+  // Hide navbar based on location
+  const isAuthRoute = location.pathname === "/login" || location.pathname === "/register";
+
+
+
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/movie/:id" element={<DetailPage />} />
-          <Route path="/saved" element={<SavedMoviesPage />} />
-        </Routes>
-      </div>
-    </Router>
-  )
+    
+    <div className="App">
+      {!isAuthRoute && <Navbar/>}
+
+      <Routes>
+
+        {/* Home Path */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+
+        {/* Auth */}
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/register" element={<RegisterPage/>} />
+
+        {/* App */}
+        <Route path="/home" element={<HomePage/>} />
+        <Route path="/movie/:id" element={<DetailPage/>} />
+        <Route path="/saved" element={<SavedMoviesPage/>} />
+
+        {/* Catch */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
+
+
+      </Routes>
+
+    </div>
+
+  );
+
 }
 
-export default App
+function App() {
+  return(
+    <Router>
+      <AppLayout />
+    </Router>
+  );
+}
+
+export default App;
